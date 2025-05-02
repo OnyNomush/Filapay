802935c5c2f2b9713fa47f4cf6cf867e95a9f03e
T470s <@athaillahdeva1109>
2025-05-02 11:13:18 +0700

    Update User

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -17,14 +17,21 @@ class UserResource extends Resource
 {
     protected static ?string $model = User::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-user-plus';
     protected static ?string $navigationGroup= 'Karyawan';
+    protected static ?string $label= 'Data User';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->minLength(3),
+                Forms\Components\TextInput::make('email')
+                    ->required()
+                    ->email()
+                    ->minLength(6),
             ]);
     }
 
@@ -32,17 +39,20 @@ class UserResource extends Resource
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('name')->searchable(),
+                Tables\Columns\TextColumn::make('role')->searchable(),
+                Tables\Columns\TextColumn::make('email')->searchable(),
             ])
             ->filters([
                 //
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
+                Tables\Actions\DeleteBulkAction::make(),
                 ]),
             ]);
     }
