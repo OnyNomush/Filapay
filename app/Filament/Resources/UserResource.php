e7f4c6cae14364073c8994650fb4a4f747caf154
T470s <@athaillahdeva1109>
2025-05-02 14:26:14 +0700

    Data User

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -13,6 +13,7 @@ use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
+
 class UserResource extends Resource
 {
     protected static ?string $model = User::class;
@@ -25,6 +26,8 @@ class UserResource extends Resource
     {
         return $form
             ->schema([
+                Forms\Components\TextInput::make('avatar_url')
+                    ->label('Foto Profile'),
                 Forms\Components\TextInput::make('name')
                     ->required()
                     ->minLength(3),
@@ -39,9 +42,11 @@ class UserResource extends Resource
     {
         return $table
             ->columns([
+                Tables\Columns\ImageColumn::make('avatar_url')->searchable()->label('Foto Profile'),
                 Tables\Columns\TextColumn::make('name')->searchable(),
                 Tables\Columns\TextColumn::make('role')->searchable(),
                 Tables\Columns\TextColumn::make('email')->searchable(),
+                Tables\Columns\TextColumn::make('created_at')->searchable(),
             ])
             ->filters([
                 //
